e097ddea34d2a4b0b4e45d14bfd4841e8bea95a9
Vanessa Souto <vansouferr@gmail.com>
2024-08-02 15:21:55 -0300

    feat: customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchCustomersPages } from '@/app/lib/data';
+import Table from "@/app/ui/customers/table";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}:{
+  searchParams?:{
+    query?:string;
+    page?:string;
+  };
+}) {
+  
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+    return(
+      <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> 
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} /> 
+      </div>
+    </div>
+    );
   }
